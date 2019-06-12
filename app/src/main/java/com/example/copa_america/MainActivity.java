d7c5417056ea246c5626b728cedaa3c89f096b01
Santiago Yepes <ysantiago@unicauca.edu.co>
2019-06-12 02:12:10 -0500

    Ready for include list view

--- app/src/main/java/com/example/copa_america/MainActivity.java
+++ app/src/main/java/com/example/copa_america/MainActivity.java
@@ -38,8 +38,8 @@ public class MainActivity extends AppCompatActivity
         viewPagerFragment = (ViewPager) findViewById(R.id.viewPagerFragments_id);
         ViewPagerFragmentAdapter adapterFragment = new ViewPagerFragmentAdapter(getSupportFragmentManager());
         //creado el objeto ViewPagerFragmentAdapter procedemos a adicionar fragments con la funcion addFragment de la clase ViewPagerFragmentAdapter
-        adapterFragment.addFragment(new FragmentGrupos(), "Grupos");
-        adapterFragment.addFragment(new FragmentEliminatorias(), "Eliminatorias");
+        adapterFragment.addFragment(new FragmentGrupos(), getString(R.string.btn1tabLayout));
+        adapterFragment.addFragment(new FragmentEliminatorias(), getString(R.string.btn2tabLayout));
         //configuracion adapterFragment al content_main(xml)
         viewPagerFragment.setAdapter(adapterFragment);
         tabLayout.setupWithViewPager(viewPagerFragment);
@@ -89,18 +89,12 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_home) {
-            mTextMessage.setText(R.string.title_home);
-        } else if (id == R.id.nav_gallery) {
-            mTextMessage.setText(R.string.menu_gallery);
-        } else if (id == R.id.nav_slideshow) {
-            mTextMessage.setText(R.string.menu_slideshow);
-        } else if (id == R.id.nav_tools) {
-            mTextMessage.setText(R.string.menu_tools);
-        } else if (id == R.id.nav_share) {
-            mTextMessage.setText(R.string.menu_share);
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.nav_qr) {
+            mTextMessage.setText(R.string.menu_qr);
+        } else if (id == R.id.nav_settings) {
+            mTextMessage.setText(R.string.menu_settings);
+        } else if (id == R.id.nav_help) {
+            mTextMessage.setText(R.string.menu_help);
         }
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
@@ -115,14 +109,14 @@ public class MainActivity extends AppCompatActivity
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_match:
+                    mTextMessage.setText(R.string.title_match);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_groups:
+                    mTextMessage.setText(R.string.title_groups);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_teams:
+                    mTextMessage.setText(R.string.title_teams);
                     return true;
             }
             return false;
