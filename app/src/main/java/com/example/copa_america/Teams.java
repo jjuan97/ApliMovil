3acbf8f95dd1755d9b8edd044e7c705b0e91a33d
Santiago Yepes <ysantiago@unicauca.edu.co>
2019-06-18 04:02:13 -0500

    Add Progress Dialog

--- app/src/main/java/com/example/copa_america/Teams.java
+++ app/src/main/java/com/example/copa_america/Teams.java
@@ -4,6 +4,10 @@ public class Teams {
 
     private int imgTeam;
     private String txtTeam;
+    private String pj;
+    private String p;
+    private String dg;
+    private String position;
 
     public Teams(){
 
@@ -12,6 +16,38 @@ public class Teams {
     public Teams(int imgTeam, String txtTeam) {
         this.imgTeam = imgTeam;
         this.txtTeam = txtTeam;
+    }
+
+    public String getPj() {
+        return pj;
+    }
+
+    public void setPj(String pj) {
+        this.pj = pj;
+    }
+
+    public String getP() {
+        return p;
+    }
+
+    public void setP(String p) {
+        this.p = p;
+    }
+
+    public String getDg() {
+        return dg;
+    }
+
+    public void setDg(String dg) {
+        this.dg = dg;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
     }
 
     public int getImgTeam() {
